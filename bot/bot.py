307008054de25e40b71a1601553f8d126a2cb2f0
timofejmihee1-cpu <timofejmihee1@gmail.com>
2026-03-26 21:36:09 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -156,6 +156,7 @@ def get_cmd(m):
             "⚠️ **ВНИМАНИЕ:**\n"
             "Некоторые прокси могут не работать из-за ограничений твоего **оператора** или **Wi-Fi**. "
             "Если не грузит — попробуй следующий из списка."
+            "подпишись на https://t.me/proxy_timoxa"
         )
         bot.edit_message_text(res, m.chat.id, wait_msg.message_id, parse_mode="Markdown")
     else:
